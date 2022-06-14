833fd0ca89afaf3e8d3f14f14cf2a90f4c7fcc9c
Cee1jay <91193206+Cee1jay@users.noreply.github.com>
2022-06-14 18:19:50 +0300

    palindrome lab

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
 }
+function isPalindrome(word) {
+  if (word.length <= 1) {
+    return true;
+  } else {
+    if (word[0] === word[word.length - 1]) {
+      return isPalindrome(word.slice(1, -1));
+    } else {
+      return false;
+    }
+  }
+}
 
 /* 
   Add your pseudocode here
